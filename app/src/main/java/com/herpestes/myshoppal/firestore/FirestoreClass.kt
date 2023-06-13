1e6fe8c2cf7465855a30f5e629f533df63776b80
herpestes <ahmetbeyazoglu@gmail.com>
2023-06-13 21:12:54 +0300

    FireStore

---
+++ app/src/main/java/com/herpestes/myshoppal/firestore/FirestoreClass.kt
@@ -0,0 +1,9 @@
+package com.herpestes.myshoppal.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreClass {
+
+    private val mFirestore = FirebaseFirestore.getInstance()
+
+}
