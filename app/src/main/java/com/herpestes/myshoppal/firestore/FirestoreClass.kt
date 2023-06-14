c0ea814987ce35c7812c29362c48a897e2953ef7
herpestes <ahmetbeyazoglu@gmail.com>
2023-06-14 20:51:20 +0300

    UserDetails

--- app/src/main/java/com/herpestes/myshoppal/firestore/FirestoreClass.kt
+++ app/src/main/java/com/herpestes/myshoppal/firestore/FirestoreClass.kt
@@ -1,12 +1,17 @@
 package com.herpestes.myshoppal.firestore
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.provider.SyncStateContract
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
+import com.herpestes.myshoppal.activities.LoginActivity
 import com.herpestes.myshoppal.activities.RegisterActivity
 import com.herpestes.myshoppal.models.User
+import com.herpestes.myshoppal.utils.Constants
 
 class FirestoreClass {
 
@@ -32,5 +37,43 @@ class FirestoreClass {
         }
         return currentUserId
     }
+
+    fun getUserDetails(activity: Activity) {
+        mFireStore.collection(Constants.USERS)
+            .document(getCurrentUserId())
+            .get()
+            .addOnSuccessListener { document ->
+                val user = document.toObject(User::class.java)
+                if (user != null) {
+
+                    val sharedPref = activity.getSharedPreferences(
+                        Constants.MY_SHOP_PAL_PREFERENCES,
+                        Context.MODE_PRIVATE
+                    )
+
+                    val editor: SharedPreferences.Editor = sharedPref.edit()
+                    editor.putString(
+                        Constants.LOGGED_IN_USERNAME,
+                        "${user.firstName} ${user.lastName}"
+                    )
+                    editor.apply()
+
+                    when (activity) {
+                        is LoginActivity -> activity.userLoggedInSuccess(user)
+                        is RegisterActivity -> activity.userLoggedInSuccess(user)
+                        is SettingsActivity -> activity.userDetailSuccess(user)
+                    }
+                }
+
+            }.addOnFailureListener { exception ->
+                when (activity) {
+                    is LoginActivity -> activity.hideProgressDialog()
+                    is RegisterActivity -> activity.hideProgressDialog()
+                    is Settings     Activity -> activity.hideProgressDialog()
+                }
+                Log.e(activity.javaClass.simpleName, exception.message.toString())
+            }
+    }
+
 
 }
