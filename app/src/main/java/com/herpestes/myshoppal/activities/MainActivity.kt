2ac6be131721a3c2b7607de68b26a7e6dc1f450b
herpestes <ahmetbeyazoglu@gmail.com>
2023-06-14 23:54:27 +0300

    userProfile

--- app/src/main/java/com/herpestes/myshoppal/activities/MainActivity.kt
+++ app/src/main/java/com/herpestes/myshoppal/activities/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.herpestes.myshoppal.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.herpestes.myshoppal.R
+import com.herpestes.myshoppal.databinding.ActivityMainBinding
+import com.herpestes.myshoppal.utils.Constants
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
+            val sharedPref = getSharedPreferences(Constants.MY_SHOP_PAL_PREFERENCES, Context.MODE_PRIVATE)
+            val userName = sharedPref.getString(Constants.LOGGED_IN_USERNAME,"Hello")
 
+            binding.userName.text = userName.toString()
+        }
     }
 }
--- app/src/main/java/com/herpestes/myshoppal/utils/Constants.kt
+++ app/src/main/java/com/herpestes/myshoppal/utils/Constants.kt
@@ -3,4 +3,14 @@ package com.herpestes.myshoppal.utils
 object Constants {
 
     const val USERS: String = "users"
+    const val PRODUCTS : String = "products"
+    const val CART_ITEMS : String = "card_items"
+    const val ORDERS : String = "orders"
+    const val SOLD_PRODUCT :String = "sold_products"
+
+    const val MY_SHOP_PAL_PREFERENCES: String = "myShopPalPreferences"
+    const val LOGGED_IN_USERNAME: String = "logged_in_user_name"
+    const val EXTRA_USER_DETAILS: String = "extra_user_details"
+    const val EXTRA_ADDRESS_DETAILS: String = "extra_address_details"
+
 }
